b15954bb21eaa90a679610b2ae7a6325a492f48f
Ramesh Vellanki <gcpt0801@gmail.com>
2025-11-19 12:42:40 +0530

    Add terraform destroy step before apply in workflow

---
+++ scripts/import-terraform-state.sh
@@ -0,0 +1,19 @@
+# Script to import existing GCP resources into Terraform state
+
+# Import the network
+terraform import google_compute_network.webapp_network projects/gcp-terraform-demo-474514/global/networks/webapp-network
+
+# Import the static IP
+terraform import google_compute_address.webapp_static_ip projects/gcp-terraform-demo-474514/regions/us-central1/addresses/random-names-webapp-instance-static-ip
+
+# Import the firewall rules
+terraform import google_compute_firewall.allow_http projects/gcp-terraform-demo-474514/global/firewalls/webapp-network-allow-http
+terraform import google_compute_firewall.allow_tomcat projects/gcp-terraform-demo-474514/global/firewalls/webapp-network-allow-tomcat
+terraform import google_compute_firewall.allow_ssh projects/gcp-terraform-demo-474514/global/firewalls/webapp-network-allow-ssh
+
+# Import the compute instance
+terraform import google_compute_instance.webapp_instance projects/gcp-terraform-demo-474514/zones/us-central1-a/instances/random-names-webapp-instance
+
+# After importing, run:
+# terraform plan
+# terraform apply
